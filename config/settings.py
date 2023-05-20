f178675a513b80fb5b7cce8c74a439aaa43ef4e1
Marcin Bojara <marcin_5@mail.ru>
2023-05-20 16:06:11 +0200

    set up postgres database

--- config/settings.py
+++ config/settings.py
@@ -82,8 +82,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": env("POSTGRES_DB_NAME"),
+        "USER": env("POSTGRES_DB_USER"),
+        "PASSWORD": env("POSTGRES_DB_PASS"),
     }
 }
 
