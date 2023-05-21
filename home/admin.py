164e60a14065040e48ae551a098ff61d5c3f0526
Marcin Bojara <marcin_5@mail.ru>
2023-05-21 23:00:38 +0200

    add Article and related models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Category, Article
+
+
+admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(Tag)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,48 @@
-from django.db import models
+import re
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def save(self, *args, **kwargs):
+        self.name = re.sub(r"[^a-zA-Z0-9]", "", self.name)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"#{self.name}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(blank=True, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    categories = models.ManyToManyField(Category, blank=True)
+
+    class Meta:
+        ordering = ["title"]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
