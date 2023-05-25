cbf7a7b7894a468619a1a1d355ec37ed22e5a8b7
Marcin Bojara <marcin_5@mail.ru>
2023-05-25 17:48:10 +0200

    show only not published contents

--- home/admin.py
+++ home/admin.py
@@ -3,7 +3,14 @@ from django.contrib import admin
 from .models import Tag, Category, Article, Content
 
 
-admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "slug":
+            kwargs["queryset"] = Content.objects.filter(published=False)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
 admin.site.register(Content)
 admin.site.register(Category)
 admin.site.register(Tag)
