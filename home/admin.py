f8dc66de0508c628c2ddc431efbe8e16af99b89a
Marcin Bojara <marcin_5@mail.ru>
2023-05-25 20:58:22 +0200

    add comment

--- home/admin.py
+++ home/admin.py
@@ -8,10 +8,12 @@ class ArticleAdmin(admin.ModelAdmin):
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "slug":
             change = request.path.endswith("/change/")
+            # show only not published contents when add new article
             kw = {"published": change}
+            # do not allow change primary key when edit
             if change:
                 kw["slug"] = request.path.split("/")[-3]
-                self.readonly_fields = ("slug", )
+                self.readonly_fields = ("slug",)
             kwargs["queryset"] = Content.objects.filter(**kw)
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
