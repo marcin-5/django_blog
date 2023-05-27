f32de0ee08e3c3cada85ef54042b219fb5b867e5
Marcin Bojara <marcin_5@mail.ru>
2023-05-27 22:13:12 +0200

    add api view for article list

--- home/api/urls.py
+++ home/api/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path("hello/", views.hello_world, name="hello_world"),
+    path("list/", views.ArticleListView.as_view()),
     path("<slug:slug>/", views.ArticleView.as_view()),
 ]
--- home/api/views.py
+++ home/api/views.py
@@ -13,3 +13,9 @@ class ArticleView(generics.RetrieveAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     lookup_field = "slug"
+
+
+class ArticleListView(generics.ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    lookup_field = "slug"
