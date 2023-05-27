a7bc621275089b82fb7017a3df6b2d9f212d3491
Marcin Bojara <marcin_5@mail.ru>
2023-05-27 22:03:49 +0200

    add filtering and ordering to article list

--- home/views.py
+++ home/views.py
@@ -15,9 +15,9 @@ class ArticleView(DetailView):
     content = None
 
     def get_context_data(self, **kwargs):
-        article = super().get_context_data(**kwargs)
-        article["content"] = self.content
-        return article
+        context = super().get_context_data(**kwargs)
+        context["content"] = self.content
+        return context
 
     def get_queryset(self):
         self.content = get_object_or_404(Content, slug=self.kwargs["slug"])
@@ -28,16 +28,36 @@ class ArticleView(DetailView):
 class ArticleListView(ListView):
     model = Article
     template_name = "home/article-list.html"
-    authors = None
+    ctx = {
+        "authors": {},
+        "order_fields": {"Date": "published", "Title": "slug__title"},
+        # default values for form fields
+        "author_id": 0,
+        "date_from": "",
+        "date_to": "",
+        "order_by": "Date",
+        "direction": "desc",
+    }
 
     def get_context_data(self, **kwargs):
-        articles = super().get_context_data(**kwargs)
-        articles["authors"] = self.authors
-        return articles
+        context = super().get_context_data(**kwargs)
+        context.update(self.ctx)
+        return context
 
     def get_queryset(self):
-        articles = Article.objects.filter(is_active=True)
-        self.authors = dict(articles.values_list("author_id", "author__username").distinct())
-        if "author" in self.kwargs:
-            articles = articles.filter(author=self.kwargs["author"])
+        self.ctx.update({key: value for key, value in self.request.POST.items() if key != "csrfmiddlewaretoken"})
+        field = self.ctx["order_fields"][self.ctx["order_by"]]
+        articles = Article.objects.filter(is_active=True).order_by("-" * (self.ctx["direction"] == "desc") + field)
+        self.ctx["authors"] = dict(articles.values_list("author_id", "author__username").distinct())
+
+        if self.ctx["date_from"]:
+            articles = articles.filter(published__gte=self.ctx["date_from"])
+        if self.ctx["date_to"]:
+            articles = articles.filter(published__lte=f'{self.ctx["date_to"]} 23:59')
+        if i := int(self.ctx["author_id"] or 0):
+            articles = articles.filter(author=i)
+
         return articles.values_list("slug", "slug__title", "author_id")
+
+    def post(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
