ea9be0313e6655325f8c1980f3bc8b30c0fe0eae
Marcin Bojara <marcin_5@mail.ru>
2023-06-04 13:51:12 +0200

    add forum models

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,31 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from home.models import Article
+from users.models import CustomUser
+
+
+class Thread(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, max_length=8)
+    slug = models.OneToOneField(Article, max_length=50, on_delete=models.CASCADE)
+    started_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    subject = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ["created"]
+
+    def __str__(self):
+        return self.subject
+
+
+class Post(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    text = models.TextField()
+
+    class Meta:
+        ordering = ["created"]
