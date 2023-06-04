31d3777def67fb0ddee00edbdfffd4a2766054de
Marcin Bojara <marcin_5@mail.ru>
2023-06-04 21:28:39 +0200

    fix forum models and add migrations

--- forum/models.py
+++ forum/models.py
@@ -1,4 +1,5 @@
 import uuid
+import string
 
 from django.db import models
 
@@ -6,9 +7,19 @@ from home.models import Article
 from users.models import CustomUser
 
 
+def base64uuid():
+    res = []
+    n = uuid.uuid4().int
+    chs = "-_" + string.ascii_lowercase + string.digits + string.ascii_uppercase
+    while n:
+        res.append(chs[n % 64])
+        n //= 64
+    return "".join(res) or chs[0]
+
+
 class Thread(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, max_length=8)
-    slug = models.OneToOneField(Article, max_length=50, on_delete=models.CASCADE)
+    id = models.CharField(primary_key=True, default=base64uuid, editable=False, max_length=22)
+    slug = models.ForeignKey(Article, max_length=50, on_delete=models.CASCADE)
     started_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -18,14 +29,19 @@ class Thread(models.Model):
         ordering = ["created"]
 
     def __str__(self):
-        return self.subject
+        return f"{self.subject} - {self.id}"
 
 
 class Post(models.Model):
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    written_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     text = models.TextField()
 
     class Meta:
         ordering = ["created"]
+
+    def __str__(self):
+        text = self.text.split(".")[0]
+        return f"{self.thread.subject} ({self.id}) - {text[:32 if (l := len(text)) > 31 else l]}"
