a1066b63d0044bfd1be4fe235268b9b9d1374c7b
Marcin Bojara <marcin_5@mail.ru>
2023-06-10 09:22:01 +0200

    add more tests for users app

--- conftest.py
+++ conftest.py
@@ -16,12 +16,39 @@ def django_db_setup():
 
 
 @pytest.fixture
-def create_user(db, django_db_setup, django_user_model):
+def test_password():
+    return "test-pass-123"
+
+
+@pytest.fixture
+def create_user(db, django_db_setup, django_user_model, test_password):
     """User instance"""
-    return django_user_model.objects.create_user(email="test_user@mail.ru", name="Test User", password="Pass1234")
+    def make_user(**kwargs):
+        kwargs["password"] = test_password
+        if "name" not in kwargs:
+            kwargs["name"] = "Test User"
+        if "email" not in kwargs:
+            kwargs["email"] = "test_user@mail.ru"
+        if "superuser" in kwargs:
+            del kwargs["superuser"]
+            return django_user_model.objects.create_superuser(**kwargs)
+        else:
+            return django_user_model.objects.create_user(**kwargs)
+    return make_user
 
 
 @pytest.fixture
 def registration(db, django_db_setup):
     """User instance"""
     return Registration.objects.create(email="test_user@test.ru")
+
+
+@pytest.fixture
+def auto_login_user(db, client, create_user, test_password):
+    def make_auto_login(user=None):
+        if user is None:
+            user = create_user()
+        client.login(username=user.email, password=test_password)
+        return client, user
+
+    return make_auto_login
--- users/tests/test_user_views.py
+++ users/tests/test_user_views.py
@@ -1,4 +1,12 @@
+import pytest
+
+from django.test import RequestFactory
 from django.urls import reverse
+
+from users.models import CustomUser
+from users.views import SendRegistrationLinkView, login_view
+
+factory = RequestFactory()
 
 
 def test_registration_page(client, db):
@@ -11,10 +19,11 @@ def test_registration_page(client, db):
 
 
 def test_login_page(client, create_user, db, django_user_model):
+    user = create_user()
     url = reverse("users:login")
-    data = {"username": create_user.email,
-            "password": create_user.password,
-            "name": create_user.name}
+    data = {"username": user.email,
+            "password": user.password,
+            "name": user.name}
     response = client.get(url)
     redirect = client.post(url, data=data)
     user = django_user_model.objects.get(email=data["username"])
@@ -26,9 +35,30 @@ def test_login_page(client, create_user, db, django_user_model):
     assert user.name == data["name"]
 
 
-def test_register_user(client, db, registration, django_user_model):
+def test_register_user(client, db, registration, django_user_model, create_user):
     url = reverse("users:register-uuid", kwargs={"uuid": registration.uuid})
     data = {"email": registration.email, "name": "Test User", "password1": "pass1234", "password2": "pass1234"}
+
+    # name too short
+    redirect = client.post(url, data={**data, **{"name": "X"}})
+    assert redirect.status_code == 200
+    assert len(django_user_model.objects.filter(email=data["email"])) == 0
+
+    # password missmatch
+    redirect = client.post(url, data={**data, **{"password1": "12341234"}})
+    assert redirect.status_code == 200
+    assert len(django_user_model.objects.filter(email=data["email"])) == 0
+
+    # password too short
+    redirect = client.post(url, data={**data, **{"password1": "1234123", "password2": "1234123"}})
+    assert redirect.status_code == 200
+    assert len(django_user_model.objects.filter(email=data["email"])) == 0
+
+    # email already registered #FIXME
+    test_user = create_user()
+    redirect = client.post(url, data={**data, **{"email": test_user.email}})
+    assert redirect.status_code == 200
+    test_user.delete()
 
     redirect = client.post(url, data=data)
     user = django_user_model.objects.get(email=data["email"])
@@ -40,3 +70,21 @@ def test_register_user(client, db, registration, django_user_model):
     assert response.status_code == 200
     assert "<h5>Login Form</h5>" in response.content.decode("UTF-8")
 
+    assert str(registration) == f"{registration.uuid} - {registration.email}"
+
+
+# @pytest.mark.django_db
+# def test_form_valid_registration(create_user):
+#     user = create_user()
+#     request = factory.post(reverse("users:registration"), data={"email": user.email})
+#     request = factory.post(reverse("users:registration"), data={"email": "a@a.pl"})
+
+
+# @pytest.mark.django_db
+# def test_form_valid_login(create_user):
+#     user = create_user()
+#     request = factory.post(reverse("users:login"), data={"username": user.email, "password": user.password})
+#     request.user = user
+#     response = login_view(request)
+#
+#     assert response.status_code == 200
